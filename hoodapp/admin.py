0ac149da3a5ef08d1125a4025e837ad10e59bbfd
Wambuilucy <wambuilucie99@gmail.com>
2020-10-31 10:47:35 +0300

    commit

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import User, Post , Business, Neighbourhood, Profile
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Business)
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
