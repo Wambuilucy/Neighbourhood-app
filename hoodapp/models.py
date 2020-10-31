032e0558ded5ab0bfec97944865a3a8e54870ee2
Wambuilucy <wambuilucie99@gmail.com>
2020-10-31 10:54:06 +0300

    made my templates

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,6 +1,128 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=144)
+    location = models.CharField(max_length=144)
+    occupants_count = models.PositiveIntegerField(default=0)
+    police_contacts =models.PositiveIntegerField(default=0)
+    health_contacts =models.PositiveIntegerField( default=0)
+    admin = models.ForeignKey(User,on_delete=models.CASCADE, blank=True , null=True)
+
+
+    def create_neighbourhood(self):
+        self.save()
+        
+    def delete_neighbourhood(self):
+        self.delete()
+        
+    @classmethod
+    def find_neighbourhood(cls,id):
+        search = cls.objects.get(id = id)
+        return  search
+    
+    @classmethod   
+    def update_neigbourhood(cls,id,new_name):
+        cls.objects.filter(pk = id).update(name=new_name)
+        new_name_object = cls.objects.get(name = new_name)
+        new_name = new_name_object.name
+        return new_name
+    
+    @classmethod
+    def update_occupants(cls,id,new_occupants):
+        cls.objects.get(pk = id).update(occupants=new_occupants)
+        new_occupants_object = cls.objects.get(pk__id = id)
+        new_occupants = new_name_object.occupants
+        return new_occupants
+    
+    def __str__(self):
+        return self.name
+
+
+
+class Business(models.Model):
+    owner = models.ForeignKey(User, related_name="owners")
+    name = models.CharField(max_length=144)
+    email = models.EmailField(max_length=144)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, default=1)
+    profile_pic = models.ImageField(upload_to='images/', default='media/images/default.jpeg')
+
+    def create_business(self):
+        self.save()
+        
+    def delete_business(self):
+        self.delete()
+        
+    @classmethod
+    def find_business(cls,name):
+        return cls.objects.filter(title__icontains=name).all()
+    
+    @classmethod   
+    def update_business(cls,id,new_name):
+        cls.objects.filter(pk = id).update(name=new_name)
+        new_name_object = cls.objects.get(name = new_name)
+        new_name = new_name_object.name
+        return new_name
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        
+        ordering = ['name']
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    neigbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE,blank=True,null=True)
+    bio = models.TextField()
+    profile_pic = models.ImageField(upload_to='images/', default='media/images/default.jpg')
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    email = models.EmailField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.name
+
+@receiver(post_save,sender=User)
+def create_profile(sender, instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    
+@receiver(post_save,sender=User)
+def save_profile(sender, instance,**kwargs):
+    instance.profile.save()
+    
+
+class Post(models.Model):
+    
+    title = models.CharField(max_length=100)
+    post = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    neigbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    post_pic = models.ImageField(upload_to="post_pics/", blank=True)
+    
+    
+    def create_post(self):
+        self.save()
+        
+    def delete_post(self):
+        self.delete()
+        
+    @classmethod   
+    def update_business(cls,id,post):
+        cls.objects.filter(pk = id).update(post=post)
+        new_post_object = cls.objects.filter(post__icontains = post)
+        new_post = new_post_object.post
+        return new_post
+    
+    @classmethod
+    def get_single_post(cls,id):
+        post = cls.objects.get(pk=id)
+        return post
+    
+    
+    def __str__(self):
+        return self.title
--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,6 +1,93 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from .models import Post, Profile, Business, Neighbourhood
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import UploadPostForm , UpdateProfileForm , UploadBusinessForm
 
-from django.shortcuts import render
 
-# Create your views here.
+def landing(request):
+    return render(request,'all-posts/landing.html')
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    context = {
+        'neighbourhoods': Neighbourhood.objects.all(),
+        
+    }
+    return render(request, 'all-posts/home.html', context)
+
+
+def profile(request, username):
+    user = User.objects.get(username = username)
+    profile = Profile.objects.get(user = user)
+    posts = Post.objects.filter(user = user)
+    return render(request, 'all-posts/profile.html', {'profile': profile, 'posts': posts})
+
+# @login_required(login_url='/accounts/login')
+def update_profile(request):
+    
+    my_prof = Profile.objects.get(user=request.user)
+    form = UpdateProfileForm(instance=request.user)
+
+# def post(request, id):
+#     if request.user.is_authenticated:
+#         user = User.objects.get(username = request.user)
+#         post = Post.objects.get(id = id)
+   
+#     return render(request, 'all-posts/post.html', {'post': post})
+
+def business(request, id):
+    if request.user.is_authenticated:
+        user = User.objects.get(username = request.user)
+        business = Business.objects.get(id = id)
+   
+    return render(request, 'all-posts/business.html', {'business': business})
+
+
+    
+def search(request):
+    if 'site' in request.GET and request.GET['site']:
+        search_term = request.GET.get('site')
+        businesses = Business.objects.filter(name__icontains = search_term)
+        message = f'{search_term}'
+        return render(request, 'all-posts/search.html', {'businesses': businesses, 'message': message})
+        
+    return render(request, 'all-posts/search.html')
+
+
+@login_required(login_url='/accounts/login/')
+def create_post(request):
+    if request.method == 'POST':
+        form = UploadPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+        return redirect('home')
+    else:
+        form = UploadPostForm()
+
+    return render(request, 'all-posts/create_post.html', {'form': form})
+
+@login_required(login_url='/accounts/login/')
+def create_business(request):
+    if request.method == 'POST':
+        form = UploadBusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user = request.user
+            business.save()
+        return redirect('home')
+    else:
+        form = UploadBusinessForm()
+
+    return render(request, 'all-posts/create_business.html', {'form': form})
+
+
+@login_required(login_url='/accounts/login/?next=/')   
+def single_neighbourhood(request,id):
+    single_neighbourhood = Neighbourhood.objects.get(pk=id)
+    business = single_neighbourhood.business_set.all
+    post = single_neighbourhood.post_set.all
+    return render (request, 'all-posts/post.html',{'neighbourhood':single_neighbourhood,'business':business,'post':post})
